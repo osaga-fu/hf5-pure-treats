e8b13a1c2d4ebe2597c5ea0493e9f47a067b572d
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-02-21 23:38:08 +0100

    create navbar content and styles

--- src/components/molecules/Navbar.jsx
+++ src/components/molecules/Navbar.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import ReactDOM from 'react-dom/client'
+import 'tailwindcss/tailwind.css';
+import './Navbar.css';
+
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <>
+   <header>
+    <div className="container">
+        <img src="\src\assets\images\pure_treats_logo.png" alt="puretreats_logo" className="puretreats_logo" />
     </div>
+    <div className="logo"></div>
+    <nav>
+        <ul>
+            <li><a href="download.html"><strong>QUIENES SOMOS</strong></a></li>
+            <li><a href="download.html"><strong>NUESTRA FILOSOFIA</strong></a></li>
+            <li><a href="download.html"><strong>CONTACTO</strong></a></li>
+            <li><a href="download.html"><strong>PARTNERS</strong></a></li>
+        </ul>
+        <button onClick={() => window.location.href='https://factoriaf5.org/'} className="button_1"> <strong>Empezar</strong></button>
+    </nav>
+</header>
+    </>
   )
 }
 
