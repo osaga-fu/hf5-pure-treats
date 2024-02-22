63a0040dd920e1d26a48c2fb50104846a9a1df2d
Helena de Rosa <149380974+HelenaDR84@users.noreply.github.com>
2024-02-22 11:03:07 +0100

    add new navbar styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from '../src/components/molecules/Navbar'
-import 'tailwindcss/tailwind.css';
 
 
 
@@ -10,6 +9,7 @@ function App() {
 
   return (
    <Navbar> </Navbar>
+
 
   )
 }
