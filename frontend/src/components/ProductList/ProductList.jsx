68b95f3a9f98ea743526a7580fa2a54e80679d27
osaga-fu <151243613+osaga-fu@users.noreply.github.com>
2024-02-22 19:24:44 +0100

    Completed product list component

--- frontend/src/components/ProductList/ProductList.jsx
+++ frontend/src/components/ProductList/ProductList.jsx
@@ -4,20 +4,13 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function ProductList() {
-  
   const [data, setData] = useState([]);
-
-  const handleSearch = async () => {
-    
-  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(
-          `http://localhost:8080/products`
-        );
-  
+        const response = await fetch(`http://localhost:8080/products`);
+
         const data = await response.json();
         setData(data);
       } catch (error) {
@@ -28,14 +21,22 @@ export default function ProductList() {
     fetchData();
   }, []);
 
-  return data.map((product) => (
-    <article className="productCard">
-      <img className="productImage" src={product.photoUrl} alt={product.name} />
-      <section className="productTextInfo">
-        <h2>{product.name}</h2>
-        <h4>{product.price} €</h4>
-        <button className="moreInfoButton">Más info</button>
-      </section>
-    </article>
-  ));
+  return (
+    <main className="cardContainer">
+      {data.map((product) => (
+        <article className="productCard" key={product.id}>
+          <img
+            className="productImage"
+            src={product.photoUrl}
+            alt={product.name}
+          />
+          <section className="productTextInfo">
+            <h2>{product.name}</h2>
+            <h4>{product.price} €</h4>
+            <button className="moreInfoButton">Más info</button>
+          </section>
+        </article>
+      ))}
+    </main>
+  );
 }
