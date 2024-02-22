9ad45e91a77428e38a492d230e3031177591288f
Ermapla <ermapla@gmail.com>
2024-02-22 11:11:51 +0100

    organize cart files estructure

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { CaritoApp } from './CaritoApp'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+
   <React.StrictMode>
+     <BrowserRouter>
+    <React.StrictMode>
+      <CaritoApp />
+    </React.StrictMode>,
+  </BrowserRouter>
+
     <App />
   </React.StrictMode>,
 )
