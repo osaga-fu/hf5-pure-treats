b5dcfc2adf977fb6638e966ecedcb6c2b1a0739b
Vila71 <lolivila71@gmail.com>
2024-02-22 12:24:57 +0100

    created method get by id

--- backend/src/main/java/com/puretreats/backend/Controllers/ProductController.java
+++ backend/src/main/java/com/puretreats/backend/Controllers/ProductController.java
@@ -2,15 +2,17 @@ package com.puretreats.backend.Controllers;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.puretreats.backend.Models.Product;
 import com.puretreats.backend.Persistence.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -30,6 +32,19 @@ public class ProductController {
                     product.isLactoseFree(), product.isSugarFree(), product.getPhotoUrl()));
         }
         return products;
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductResponse> getProductById(@PathVariable Integer id) {
+        Optional<Product> optionalProduct = productRepository.findById(id);
+        if (optionalProduct.isPresent()) {
+            Product existingProduct = optionalProduct.get();
+            ProductResponse productResponse = new ProductResponse(existingProduct.getId(),existingProduct.getName() ,existingProduct.getPrice(),existingProduct.getIngredients(),existingProduct.getnutritionalInformation(),existingProduct.isGlutenFree(),existingProduct.isLactoseFree(),existingProduct.isSugarFree(),existingProduct.getPhotoUrl(),     
+            );
+            return ResponseEntity.ok(productResponse);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
